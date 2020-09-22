2c8398383ded2c6a33dd463e9805bb7b7fb722a4
Becky <becky.gagnon92@gmail.com>
2020-09-21 19:07:52 -0700

    Complete board through diagonal conflicts

--- src/Board.js
+++ src/Board.js
@@ -123,12 +123,28 @@
     //
     // test if a specific major diagonal on this board contains a conflict
     hasMajorDiagonalConflictAt: function(majorDiagonalColumnIndexAtFirstRow) {
-      return false; // fixme
+      var row = 0;
+      var col = majorDiagonalColumnIndexAtFirstRow;
+      while(!this._isInBounds(row, col)) {
+        row++;
+        col++;
+      }
+      var count = 0;
+      while (this._isInBounds(row, col)) {
+        if(this.attributes[row][col] === 1) { count++; }
+        row++;
+        col++;
+      }
+      return (count > 1);
     },
 
     // test if any major diagonals on this board contain conflicts
     hasAnyMajorDiagonalConflicts: function() {
-      return false; // fixme
+      for (var i = -this.attributes.n + 1; i < this.attributes.n; i++) {
+        if(this.hasMajorDiagonalConflictAt(i)) { return true; }
+      }
+      return false;
+
     },
 
 
@@ -138,12 +154,27 @@
     //
     // test if a specific minor diagonal on this board contains a conflict
     hasMinorDiagonalConflictAt: function(minorDiagonalColumnIndexAtFirstRow) {
-      return false; // fixme
+      var row = 0;
+      var col = minorDiagonalColumnIndexAtFirstRow;
+      while(!this._isInBounds(row, col)) {
+        row++;
+        col--;
+      }
+      var count = 0;
+      while (this._isInBounds(row, col)) {
+        if(this.attributes[row][col] === 1) { count++; }
+        row++;
+        col--;
+      }
+      return (count > 1);
     },
 
     // test if any minor diagonals on this board contain conflicts
     hasAnyMinorDiagonalConflicts: function() {
-      return false; // fixme
+      for (var i = this.attributes.n * 2 - 2; i >= 0; i--) {
+        if(this.hasMinorDiagonalConflictAt(i)) { return true; }
+      }
+      return false;
     }
 
     /*--------------------  End of Helper Functions  ---------------------*/
